4d9c5713eee9f8dd6f73354beb1769c908677253
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-09 21:15:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@
 name="$USER"
 
 sudo apt install dos2unix --yes
-dos2unix *.sh ./Scripts/*.sh
-chmod 700 *.sh ./Scripts/*.sh
+dos2unix *.sh ./Scripts/*.sh ./Installscripts/*.sh
+chmod 700 *.sh ./Scripts/*.sh ./Installscripts/*.sh
 sudo sed -i "s/Include /etc/ssh/sshd_config.d/*.conf/#Include /etc/ssh/sshd_config.d/*.conf/g" /etc/ssh/sshd_config
 mv Scripts /home/$name/Scripts
 sh ./Installscripts/apt_snap_block.sh
