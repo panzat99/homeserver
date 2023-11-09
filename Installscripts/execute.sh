8dbc7df614541502d542a88cd251cca9b8001216
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-09 21:38:55 +0100

    Update execute.sh

--- Installscripts/execute.sh
+++ Installscripts/execute.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+sh ./Installscripts/snap_block.sh
+sh ./Installscripts/homeserver_setup_crontab.sh $1
+sh ./Installscripts/homeserver_setup_syncthing.sh $1
+sh ./Installscripts/homeserver_setup_wireguard.sh $1
+sh ./Installscripts/homeserver_setup_ufw.sh $1
+sh ./Installscripts/homeserver_setup_docker.sh $1
