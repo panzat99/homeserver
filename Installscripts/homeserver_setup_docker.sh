4ff4e6b16852f83df70a05465fca53cebd3d4856
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-09 21:17:12 +0100

    Update homeserver_setup_docker.sh

--- Installscripts/homeserver_setup_docker.sh
+++ Installscripts/homeserver_setup_docker.sh
@@ -8,7 +8,7 @@ sudo systemctl enable containerd.service
 sudo systemctl start docker.service
 sudo systemctl start containerd.service
 git clone https://github.com/panzat99/Docker.git
-mv Docker /home/$1/docker-server
+mv ./Docker /home/$1/docker-server
 mv /home/$1/docker-server/env /home/$1/docker-server/.env
 chmod 600 /home/$1/docker-server/.env
 newgrp docker
