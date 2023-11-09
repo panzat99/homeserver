62cd71e591db2feb6c152438dfd5ee0626a7ba62
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 00:15:04 +0100

    Update file_backup.sh

--- Scripts/file_backup.sh
+++ Scripts/file_backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo mount PARTUUID="" /home/PLACEHOLDER/Sync_backup
-rsync -ac --delete --update /home/PLACEHOLDER/Sync/* /home/PLACEHOLDER/Sync/Sync_backup
-chown -R PLACEHOLDER:PLACEHOLDER /home/PLACEHOLDER/Sync_backup
-sudo umount /home/PLACEHOLDER/Sync_backup
+sudo mount PARTUUID="" /mnt
+rsync -ac --delete --update /home/PLACEHOLDER/Sync/* /mnt
+chown -R PLACEHOLDER:PLACEHOLDER /mnt/*
+sudo umount /mnt
 rsync -ac --delete --update /home/PLACEHOLDER/Sync/* /docker/nc/data/backup/files
 chown -R www-data:www-data /docker/nc/data/backup/files/
 sh /home/PLACEHOLDER/Scripts/database_update.sh backup
