4bd9aa7136653608d68b9bb20c0e53dcd22da99c
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 10:37:31 +0100

    Update docker.sh

--- Installscripts/docker.sh
+++ Installscripts/docker.sh
@@ -3,12 +3,8 @@
 sudo apt install docker docker-compose --yes
 sudo groupadd docker
 sudo usermod -aG docker $1
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl start docker.service
-sudo systemctl start containerd.service
+sudo systemctl enable docker.service; sudo systemctl enable containerd.service; sudo systemctl start docker.service; sudo systemctl start containerd.service
 git clone https://github.com/panzat99/docker.git
-mv ./docker /home/$1/docker-server
-mv /home/$1/docker-server/env /home/$1/docker-server/.env
+mv ./docker /home/$1/docker-server; mv /home/$1/docker-server/env /home/$1/docker-server/.env
 chmod 600 /home/$1/docker-server/.env
 newgrp docker
