cc2dc534e4c20c58d8b1324089e3da66d3a8313b
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 10:30:35 +0100

    Update ssh_config.sh

--- Installscripts/ssh_config.sh
+++ Installscripts/ssh_config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo sed -i "s/Include /etc/ssh/sshd_config.d/*.conf/#Include /etc/ssh/sshd_config.d/*.conf/g" /etc/ssh/sshd_config
+sudo echo -e "\nChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
