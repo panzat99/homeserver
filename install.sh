46c6c2b6946914e7dca50d2b4e4ea9782834e424
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 10:10:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@
 name="$USER"
 
 sudo apt install dos2unix --yes
-dos2unix *.sh ./Scripts/*.sh ./Installscripts/*.sh
-chmod 700 *.sh ./Scripts/*.sh ./Installscripts/*.sh
-sudo sed -i "s/Include /etc/ssh/sshd_config.d/*.conf/#Include /etc/ssh/sshd_config.d/*.conf/g" /etc/ssh/sshd_config
+dos2unix ./Scripts/*.sh ./Installscripts/*.sh
+chmod 700 ./Scripts/*.sh ./Installscripts/*.sh
 mv Scripts /home/$name/Scripts
 sh ./Installscripts/execute.sh $name
