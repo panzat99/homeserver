4d12b2688f0d77c4f62a33b7590d33185bf520fd
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 10:41:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,5 @@ sudo apt install dos2unix --yes
 dos2unix ./Scripts/*.sh ./Installscripts/*.sh
 chmod 700 ./Scripts/*.sh ./Installscripts/*.sh
 mv Scripts /home/$name/Scripts
+sudo apt remove --autoremove dos2unix --yes
 sh ./Installscripts/_execute.sh $name
