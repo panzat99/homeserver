91e1f798c81c7b452588b72a094cdef619061c94
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 10:41:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Terminal input the PARTUUIDs from the partitions in order for Sync, Sync_backup, /docker
-#
-# homeserver_setup_docker.sh needs to be executed last because newgrp subshell allows
-# to start docker containers and let them run when the shell is terminated
-
 name="$USER"
 
 sudo apt install dos2unix --yes
