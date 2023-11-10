1802d335b42e6c7ebd52196ab3ace999cc06e70e
panzat99 <86272117+panzat99@users.noreply.github.com>
2023-11-10 10:05:13 +0100

    Update homeserver_setup_crontab.sh

--- Installscripts/homeserver_setup_crontab.sh
+++ Installscripts/homeserver_setup_crontab.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sed -i "s/PLACEHOLDER/$1/g" cronjobs.txt
+sed -i "s/PLACEHOLDER/$1/g" ./Additional_Files/cronjobs.txt
+sed -i "s/PLACEHOLDER/$1/g" ./Scripts/file_backup.sh
 sudo crontab ./Additional_Files/cronjobs.txt
