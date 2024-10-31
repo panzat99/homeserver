a9e951b2d0a8144eed59fc3cc98d9dc729cc01eb
panzat99 <86272117+panzat99@users.noreply.github.com>
2024-10-31 17:54:23 +0100

    Update database_update.sh
    Updating database based on the current Syncfolder

--- Scripts/database_update.sh
+++ Scripts/database_update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker exec -d -u www-data nextcloud /var/www/html/occ files:scan -p "$1/files"
+rsync -ac --delete --update /home/hs/Sync/* /docker/nc/data/"$1"/files
+chown -R www-data:www-data /docker/nc/data/"$1"/files/ # Neccessary to interact with files e.g. move, delete, ...
+docker exec -d -u www-data nextcloud /var/www/html/occ files:scan -p "$1/files" # Core command to update the database from locally moved files
